e32e43c000809232df545fc8f36e885b93eabc7f
rodolfo frainer <rodolfofrainer@gmail.com>
2025-03-14 11:01:28 +0100

    refactor dicesContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import { JSX } from "react";
 
 import TopBar from "./components/TopBar";
-import DicesContainer from "./components/DicesContainer";
 
 export default function App(): JSX.Element {
+  // Dices Logic
   const NUMBER_OF_DICES: number = 10;
   let diceList: number[] = [];
   for (let i = 0; i < NUMBER_OF_DICES; i++) {
     diceList.push(Math.floor(Math.random() * 6));
   }
+  const dices: JSX.Element[] = diceList.map((item, index) => (
+    <button className="dice" key={index}>
+      {item}
+    </button>
+  ));
 
   return (
     <main>
       <div className="app-container">
         <TopBar />
-        <DicesContainer diceList={diceList} />
+        <div className="dices-container">{dices}</div>
+        <button className="roll-again-button">Roll Again</button>
       </div>
     </main>
   );
